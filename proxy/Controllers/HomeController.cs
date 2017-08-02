e2cd58c70f50370d85aaac72f3c74dd2747de950
fasigpt <fasigpt@gmail.com>
2017-08-02 18:59:00 +0530

    i
    test

---
+++ proxy/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics.Tracing;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using proxy;
+
+namespace proxy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> proxy()
+        {
+            _logger.LogWarning("calling outoging call");
+            var task = downloadFile();
+            string data = await task;
+            ViewData["Message"] = data;
+            return View();
+        }
+
+        public async Task<String> downloadFile()
+        {
+
+            var httpClientHandler = new HttpClientHandler
+            {
+                Proxy = new MyHttpProxy(),
+                UseProxy = true,
+                UseDefaultCredentials = true,                
+
+            };                      
+
+             var client = new HttpClient(httpClientHandler);            
+             String httpResponse = await client.GetStringAsync("http://yourproxyserviceURI");
+             return httpResponse;
+                       
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ proxy/proxy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace proxy
+{
+    public class MyHttpProxy : IWebProxy
+    {
+
+        public MyHttpProxy()
+        {
+            this.ProxyUri = new Uri("http://127.0.0.1:8888"); //Fiddler Proxy
+        }
+
+        public Uri ProxyUri { get; set; }
+
+        public ICredentials Credentials { get; set; }
+
+        public Uri GetProxy(Uri destination)
+        {
+            return this.ProxyUri;
+        }
+
+        public bool IsBypassed(Uri host)
+        {
+            //you can proxy all requests or implement bypass urls based on config settings
+            return false;
+
+        }
+    }
+}
